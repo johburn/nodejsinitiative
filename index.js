cb26c06409b2d7ec5d4d89877a0ae70e7b56c4ca
Jonathan Burmester <jonathanburmester@Jonathans-MacBook-Pro.local>
2020-09-01 09:37:10 -0500

    challenge completed

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 const express = require('express')
+const hbs = require('hbs')
+const dependencies = require('./package.json')
+const bent = require('bent')
+const semver = require('semver')
+
 
 const app = express()
+
+const getJSON = bent('json')
+
+app.set('view engine', 'hbs');
+
+app.get('/dependencies', (req, res) => {
+    res.render('dependencies.hbs', dependencies)
+})
+
+app.get('/minimum-secure', async (req, res) => {
+    let json = await getJSON('https://nodejs.org/dist/index.json')
+    //to filter
+    const filteredJSON = json.filter(x => x.security)
+        .reduce((previous, current) => (
+            !previous['v' + semver.major(current.version)] ?
+            previous['v' + semver.major(current.version)] = current :
+            null, previous
+        ), {})
+    //to order output
+    const ordered = Object.keys(filteredJSON).sort((a, b) => {
+        return semver.major(semver.valid(semver.coerce(a))) - semver.major(semver.valid(semver.coerce(b)))
+    }).reduce((obj, key) => {
+        obj[key] = filteredJSON[key]
+        return obj
+    }, {})
+
+    res.send(ordered).status(200)
+})
+
+app.get('/latest-releases', async (req, res) => {
+    let json = await getJSON('https://nodejs.org/dist/index.json')
+    //to filter
+    const filteredJSON = json.reduce((previous, current) => (
+        !previous['v' + semver.major(current.version)] ?
+        previous['v' + semver.major(current.version)] = current :
+        null, previous
+    ), {})
+
+    res.send(filteredJSON).status(200)
+})
+
 const PORT = 3000
 
 app.listen(PORT)
+
+module.exports = app
--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
 const tape = require('tape')
 const bent = require('bent')
 const getPort = require('get-port')
-
+const nock = require('nock')
 const server = require('../')
-
 const getJSON = bent('json')
 const getBuffer = bent('buffer')
 
 // Use `nock` to prevent live calls to remote services
 
-const context = {}
+let context = {}
 
 tape('setup', async function (t) {
 	const port = await getPort()
@@ -22,8 +21,61 @@ tape('setup', async function (t) {
 tape('should get dependencies', async function (t) {
 	const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
 
-	// assertions etc
+	t.equal(html.toString().includes('bent'), true, 'should contain bent')
+	t.equal(html.toString().includes('express'), true, 'should contain express')
+	t.equal(html.toString().includes('hbs'), true, 'should contain hbs')
+	t.end()
 })
+
+tape('should get minimum secure versions', async function (t) {
+	nock('https://nodejs.org/')
+		.get('/dist/index.json')
+		.reply(200, [{
+			"version": "v0.12.17",
+			"security": true
+		}, {
+			"version": "v0.12.16",
+			"security": true
+		}, {
+			"version": "v4.9.1",
+			"security": false
+		}, {
+			"version": "v4.9.0",
+			"security": true
+		}])
+
+	const response = (await getJSON(`${context.origin}/minimum-secure`))
+
+	t.equal(response.v0.version, 'v0.12.17', 'v0 version should match')
+	t.equal(response.v4.version, 'v4.9.0', 'v4 version should match')
+
+})
+
+tape('should get latest-releases', async function (t) {
+	nock('https://nodejs.org/')
+		.get('/dist/index.json')
+		.reply(200, [{
+			"version": "v14.9.0",
+			"security": true
+		}, {
+			"version": "v14.4.0",
+			"security": true
+		}, {
+			"version": "v13.14.0",
+			"security": false
+		}, {
+			"version": "v13.11.0",
+			"security": true
+		}])
+
+	const response = (await getJSON(`${context.origin}/latest-releases`))
+
+	t.equal(response.v14.version, 'v14.9.0', 'v14 version should match')
+	t.equal(response.v13.version, 'v13.14.0', 'v13 version should match')
+
+})
+
+
 
 // more tests
 
